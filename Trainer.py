4bfc0fb59ef7e0672cca90e4d3cc3153cb823373
miguelgambalan <155936957+miguelgambalan@users.noreply.github.com>
2024-07-01 20:57:52 -0400

    Update Trainer.py

--- Trainer.py
+++ Trainer.py
@@ -1,5 +1,6 @@
 from IPython.core.debugger import set_trace
-
+import pandas as pd
+import numpy as np
 from AZK import X_train
 from Helper import formatPrice, getState, plot_behavior
 from agent import Agent
@@ -30,14 +31,14 @@ for e in range(episode_count + 1):
         if action == 1: # buy
             agent.inventory.append(data[t])
             states_buy.append(t)
-            #print("Buy: " + formatPrice(data[t]))
+            print("Buy: " + formatPrice(data[t]))
 
         elif action == 2 and len(agent.inventory) > 0: # sell
             bought_price = agent.inventory.pop(0)      
             reward = max(data[t] - bought_price, 0)
             total_profit += data[t] - bought_price
             states_sell.append(t)
-            #print("Sell: " + formatPrice(data[t]) + " | Profit: " + formatPrice(data[t] - bought_price))
+            print("Sell: " + formatPrice(data[t]) + " | Profit: " + formatPrice(data[t] - bought_price))
 
         done = True if t == l - 1 else False
         #appends the details of the state action etc in the memory, which is used further by the exeReply function
@@ -49,7 +50,7 @@ for e in range(episode_count + 1):
             print("Total Profit: " + formatPrice(total_profit))
             print("--------------------------------")
             #set_trace()
-            #pd.DataFrame(np.array(agent.memory)).to_csv("Agent"+str(e)+".csv")
+            pd.DataFrame(np.array(agent.memory)).to_csv("Agent"+str(e)+".csv")
             #Chart to show how the model performs with the stock goin up and down for each 
             plot_behavior(data,states_buy, states_sell, total_profit)
         if len(agent.memory) > batch_size:
